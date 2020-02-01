738225a84be49d5ef75f0a6cfcef39f22b1dcf4b
horoiwa <horoiwa195@gmail.com>
2020-02-01 23:59:51 +0900

    Finish scatter

--- campbell/dev/app.js
+++ campbell/dev/app.js
@@ -3,13 +3,14 @@
 const width = 430;
 const height = 430;
 const margin = {top: 20, right: 20, bottom: 60, left: 60};
-const fontsize = 12
-const fontfamily = "sans-serif"
+const fontsize = 10; 
+const fontfamily = "sans-serif";
 
 const chart_width = width - margin.left - margin.right;
 const chart_height = height - margin.top - margin.bottom;
 
 const rawData = {"CRIM":{"CRIM":1.0,"ZN":-0.2004692197,"INDUS":0.4065834114,"CHAS":-0.0558915822},"ZN":{"CRIM":-0.2004692197,"ZN":1.0,"INDUS":-0.5338281863,"CHAS":-0.0426967193},"INDUS":{"CRIM":0.4065834114,"ZN":-0.5338281863,"INDUS":1.0,"CHAS":0.0629380275},"CHAS":{"CRIM":-0.0558915822,"ZN":-0.0426967193,"INDUS":0.0629380275,"CHAS":1.0}}
+const scatterData = [{"CRIM": 1,"ZN": 1,"INDUS": 1,"CHAS": 1},{"CRIM": 2,"ZN": 2,"INDUS": 2, "CHAS": 2}, {"CRIM": 3, "ZN": 3, "INDUS": 3, "CHAS": 3}]
 const indices = Object.keys(rawData)
 //
 
@@ -45,14 +46,6 @@ for (let i=0; i<indices.length; i++){
 }
 
 // Create Scaler
-var size = d3.scaleSqrt()
-    .domain([0, 1])
-    .range([0, 9]);
-
-var color = d3.scaleLinear()
-    .domain([-1, 0, 1])
-    .range(["#B22222", "#fff", "#000080"]);
-
 var x_scale = d3.scaleBand()
     .domain(indices)
     .range([0, chart_width]);
@@ -60,6 +53,14 @@ var x_scale = d3.scaleBand()
 var y_scale = d3.scaleBand()
     .domain(indices)
     .range([0, chart_height]);
+
+var csize = d3.scaleSqrt()
+    .domain([0, 1])
+    .range([x_scale.bandwidth()/10, x_scale.bandwidth()/4]);
+
+var ccolor = d3.scaleLinear()
+    .domain([-1, 0, 1])
+    .range(["#B22222", "#fff", "#000080"]);
 
 
 // Create SVG Element
@@ -90,16 +91,75 @@ svg.append("g")
 svg.selectAll(".fname")
     .data(middleData)
     .enter()
-    .append("circle")
+    .append("text")
     .attr("class", "fname")
+    .text((d) => {
+        if (d.x.length < 7){
+            return d.x;
+            }
+        else{
+            return d.x.slice(0, 7);
+            }
+        })
+    .attr("x", (d) => {
+        return x_scale(d.x) + x_scale.bandwidth()/2;
+    })
+    .attr("y", (d)=>{
+        return y_scale(d.y) + y_scale.bandwidth()/2;
+    })
+    .style("fill", "dimgrey")
+    .style("text-anchor", "middle")
+    .style("font-size", fontsize)
+    .style('font-weight', 'bold');
+
+svg.selectAll(".corCircle")
+    .data(lowerData)
+    .enter()
+    .append("circle")
+    .attr("class", "corCircle")
     .attr("cx", (d) => {
         return x_scale(d.x) + x_scale.bandwidth()/2;
     })
     .attr("cy", (d)=>{
         return y_scale(d.y) + y_scale.bandwidth()/2;
     })
-    .attr("fill", "lightgrey")
-    .attr("r", x_scale.bandwidth()/4)
+    .attr("fill", (d) => {
+        return ccolor(d.corr);
+    })
+    .attr("r", (d) => {
+        return csize(Math.abs(d.corr));
+    })
+    .on("mouseover", function(d){
+        d3.select(this)
+            .style("fill", "black")
+    });
+
+
+svg.selectAll(".corstr")
+    .data(upperData)
+    .enter()
+    .append("text")
+    .attr("class", "corstr")
+    .text((d) => {
+        console.log(d.corr);
+        return d.corr.toFixed(2);
+    })
+    .attr("x", (d) => {
+        return x_scale(d.x) + x_scale.bandwidth()/2;
+    })
+    .attr("y", (d)=>{
+        return y_scale(d.y) + y_scale.bandwidth()/2;
+    })
+    .style("fill", (d) => {
+        if (d.corr >= 0){
+            return "darkred";
+        } else {
+            return "royalblue";
+        }
+    })
+    .style("text-anchor", "middle")
+    .style("font-size", fontsize)
+    .style('font-weight', 'bold');
 
 // Create chart2
 var svg = d3.select("#chart2")
